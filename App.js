416c2d94442bc3a87ce87738d99d3b86895b459c
unjokerino <soverinLie@gmail.com>
2020-04-03 17:27:48 +0500

    done

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React,{useEffect,useState} from 'react'
-import { View, StyleSheet, Dimensions,ActivityIndicator } from "react-native";
+import { View, StyleSheet, Dimensions,ActivityIndicator, AsyncStorage } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import * as Font from 'expo-font';
+import { AppLoading, Updates, Notifications  } from "expo";
 import {
   createDrawerNavigator,
   DrawerContentScrollView,
@@ -11,6 +12,23 @@ import {
 import { TouchableOpacity } from "react-native-gesture-handler";
 import AppNavigator from "./navigation/AppNavigator" 
 import { Modal, Portal, Text, Button, Provider } from 'react-native-paper';
+import * as Permissions from 'expo-permissions';
+import * as firebase from 'firebase';
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHtsweqDfruO6JhZBxaQvkG-NPaBqTcHs",
+  authDomain: "kinoafisha-d29d7.firebaseapp.com",
+  databaseURL: "https://kinoafisha-d29d7.firebaseio.com",
+  projectId: "kinoafisha-d29d7",
+  storageBucket: "kinoafisha-d29d7.appspot.com",
+  messagingSenderId: "1080891018380",
+  appId: "1:1080891018380:web:7224710c052df32b83ffa9",
+  measurementId: "G-SNP0W2B5N6"
+};
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 
 export default function App() {
@@ -23,9 +41,34 @@ export default function App() {
       'Roboto-Thin': require('./assets/fonts/Roboto-Thin.ttf'),
     }).then(() => setLoaded(true))
     checkForUpdates()
-    
+    registerNotifications()
   }, [])
 
+
+  async function registerNotifications(){
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    // only asks if permissions have not already been determined, because
+    // iOS won't necessarily prompt the user a second time.
+    // On Android, permissions are granted on app installation, so
+    // `askAsync` will never prompt the user
+  
+    // Stop here if the user did not grant permissions
+    if (status !== 'granted') {
+      alert('No notification permissions!');
+      return;
+    }
+  
+    // Get the token that identifies this device
+    let token = await Notifications.getExpoPushTokenAsync();
+    storeHighScore(Constants.installationId,token)
+  }
+
+  function storeHighScore(userId, token) {
+    firebase.database().ref('apps/museumApp/users/' + userId).set({
+      token: token,
+      appOwnership: Constants.appOwnership
+    });
+  }
 
   async function checkForUpdates() {
     try {
