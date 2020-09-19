800203dddac797c3e4f917fbcb9da3703deb8961
Unjokerino <35525106+Unjokerino@users.noreply.github.com>
2020-09-19 17:20:33 +0300

    ticket api change

--- screens/BuyTicketScreen.js
+++ screens/BuyTicketScreen.js
@@ -3,16 +3,16 @@ import { StyleSheet, Text, View } from 'react-native'
 import { WebView } from 'react-native-webview';
 
 const BuyTicketScreen = (props) => {
-    
+
     const token = props.route.params.token
     const event = props.route.params.event
+    console.log(`https://ticketscloud.com/v1/widgets/common/meta?meta_event=${event}&token=${token}&m=w&lang=ru&utm=5e5f8c208b7305ea2ef41ab8`)
     return (
-       
-            <WebView source={{ uri: `https://v2.ticketscloud.org/v1/widgets/common/meta?meta_event=${event}&token=${token}&m=w&lang=ru&utm=5e5f8c208b7305ea2ef41ab8` }} style={{flex:1,height:'100%',width:'100%'}} />
-     
+
+        <WebView source={{ uri: `https://ticketscloud.com/v1/widgets/common/meta?meta_event=${event}&token=${token}&m=w&lang=ru&utm=5e5f8c208b7305ea2ef41ab8` }} style={{ flex: 1, height: '100%', width: '100%' }} />
+
     )
 }
 
 export default BuyTicketScreen
 
-const styles = StyleSheet.create({})
